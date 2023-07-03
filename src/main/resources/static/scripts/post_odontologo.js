36273bcf1a6f9b926307c10eec0cf95c36e577b2
sofiasastre <sofisaspa@gmail.com>
2023-07-03 00:40:33 -0300

    20230703 Versión aceptable 1

--- src/main/resources/static/scripts/post_odontologo.js
+++ src/main/resources/static/scripts/post_odontologo.js
@@ -1,7 +1,8 @@
 window.addEventListener('load', function(){
-    const formulario = document.querySelector('#add_new_estudiante');
+    const formulario = document.querySelector('#add_new_odontologo');
     formulario.addEventListener('submit', function(event){
         const formData = {
+            matricula: document.querySelector('#matricula').value,
             nombre: document.querySelector('#nombre').value,
             apellido: document.querySelector('#apellido').value,
         };
@@ -18,14 +19,15 @@ window.addEventListener('load', function(){
         fetch(url, settings)
             .then(response => response.json())
             .then(data => {
-                let succeAlert = '<div class:"alert alert-success alert-dismissible">'+'<button type="button" class="close" data-dismiss="alert">&times;</button>'+'<strong></strong>Estudiante agregado </div>'
+                let successAlert = '<div class="alert alert-success alert-dismissible">'+'<button type="button" class="close" data-dismiss="alert">&times;</button>'+'<strong></strong>Odontologo agregado </div>'
 
                 document.querySelector('#response').innerHTML = successAlert;
                 document.querySelector('#response').style.display = "block";
                 resetUploadForm();
             })
             .catch(error => {
-                let errorAlert = '<div class:"alert alert-danger alert-dismissible">'+'<button type="button" class="close" data-dismiss="alert">&times;</button>'+'<strong></strong>Error intente nuevamente</div>'
+                let errorAlert = '<div class="alert alert-danger alert-dismissible">'+
+                '<button type="button" class="close" data-dismiss="alert">&times;</button>'+'<strong></strong>Error intente nuevamente</div>'
 
                 document.querySelector('#response').innerHTML = errorAlert;
                 document.querySelector('#response').style.display = "block";
@@ -34,15 +36,16 @@ window.addEventListener('load', function(){
     });
 
     function resetUploadForm(){
+        document.querySelector('#matricula').value = "";
         document.querySelector('#nombre').value = "";
         document.querySelector('#apellido').value = "";
     }
     (function(){
         let pathname = window.location.pathname;
         if(pathname === "/"){
-            document.querySelector("nav .nav-item a:first").addClass("active");
+            document.querySelector("nav .nav-item a:first").classList.add("active");
         }else if(pathname === "/odontologoList.html"){
-            document.querySelector("nav .nav-item a:last").addClass("active");
+            document.querySelector("nav .nav-item a:last").classList.add("active");
         }
     })();
 
